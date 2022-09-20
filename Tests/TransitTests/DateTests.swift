7173d7fee3aa31b3d6448a67f3487d5ce55cdec1
Soroush Khanlou <soroushkhanlou@gmail.com>
2022-09-19 23:07:32 -0400

    add single date tests

--- Tests/TransitTests/DateTests.swift
+++ Tests/TransitTests/DateTests.swift
@@ -12,7 +12,7 @@ import XCTest
 final class DateTests: XCTestCase {
 
     func testSimpleMillisecondsSince1970Date() throws {
-        // one_date.json
+        // date_in_map.json
         let data = """
             ["^ ", "~:date", "~m946728000000"]
         """
@@ -28,7 +28,7 @@ final class DateTests: XCTestCase {
     }
 
     func testSimpleISO8601Date() throws {
-        // one_date.json
+        // dates_interesting.verbose.json
         let data = """
             ["~t1776-07-04T12:00:00.000Z","~t1970-01-01T00:00:00.000Z","~t2000-01-01T12:00:00.000Z","~t2014-04-07T22:17:17.000Z"]
         """
@@ -41,4 +41,15 @@ final class DateTests: XCTestCase {
         XCTAssertEqual(decoded[2], Date(timeIntervalSince1970: 946728000))
     }
 
+    func testSingleDate() throws {
+        // one_date.verbose.json
+        let data = """
+        ["~#'","~m946728000000"]
+        """
+            .data(using: .utf8)!
+
+        let decoded = try TransitDecoder().decode(Date.self, from: data)
+
+        XCTAssertEqual(decoded, Date(timeIntervalSince1970: 946728000))
+    }
 }
