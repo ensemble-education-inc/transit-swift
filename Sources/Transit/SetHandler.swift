e8d09240c5c4fce7e10ab6388db8906d95e69a44
Soroush Khanlou <soroushkhanlou@gmail.com>
2022-09-30 09:23:34 -0400

    when deleting a key, make sure to cache it

--- Sources/Transit/SetHandler.swift
+++ Sources/Transit/SetHandler.swift
@@ -8,13 +8,14 @@
 import Foundation
 
 public struct SetHandler: Handler {
-    public func transform(value: Any, context: inout Context) -> Any {
+    public func transform(value: Any, context: inout Context) throws -> Any {
         guard let array = value as? [Any] else {
             return value
         }
         guard array.first as? String == "~#set" else {
             return value
         }
+        _ = try context.transform(value: array[0])
         return array[1]
     }
 }
--- Tests/TransitTests/KeywordCachingTests.swift
+++ Tests/TransitTests/KeywordCachingTests.swift
@@ -45,4 +45,23 @@ final class KeywordCachingTests: XCTestCase {
         XCTAssertEqual(decoded.result.curriculums.first?.id, 43)
         XCTAssertEqual(decoded.result.curriculums.first?.readableID, "learning-path")
     }
+
+    func testCachingKeywordsAsValuesInMaps() throws {
+
+        let data = """
+            [\"^ \",\"~:result\",[\"^ \",\"~:named-expressions\",[\"~#list\",[[\"^ \",\"~:id\",1,\"~:name\",\"Starts with a I chord\",\"~:expression\",\"~^I,\",\"~:kind\",\"~:regex\",\"~:location\",null,\"~:scope\",\"~:roman-triads\"],[\"^ \",\"^3\",2,\"^4\",\"Starts with a II chord\",\"^5\",\"~^II,\",\"^6\",\"^7\",\"^8\",null,\"^9\",\"^:\"],[\"^ \",\"^3\",3,\"^4\",\"Starts with a IIm chord\",\"^5\",\"~^IIm,\",\"^6\",\"^7\",\"^8\",null,\"^9\",\"^:\"],[\"^ \",\"^3\",9,\"^4\",\"First Chord\",\"^5\",\"~^([^,]+)\",\"^6\",\"^7\",\"^8\",null,\"^9\",\"^:\"],[\"^ \",\"^3\",14,\"^4\",\"I, I7, IV\",\"^5\",\"~^I,I7,IV\",\"^6\",\"^7\",\"^8\",null,\"^9\",\"~:roman-tetrachords\"],[\"^ \",\"^3\",17,\"^4\",\"Has a Im chord\",\"^5\",\"(?:^|[,])Im(?:$|[,])\",\"^6\",\"^7\",\"^8\",null,\"^9\",\"^:\"],[\"^ \",\"^3\",18,\"^4\",\"Starts with IIIm\",\"^5\",\"~^IIIm,\",\"^6\",\"^7\",\"^8\",null,\"^9\",\"^:\"],[\"^ \",\"^3\",19,\"^4\",\"I, Vm\",\"^5\",\"~^I,Vm,\",\"^6\",\"^7\",\"^8\",null,\"^9\",\"^:\"],[\"^ \",\"^3\",20,\"^4\",\"I,Vm,I,IV\",\"^5\",\"~^I,Vm,I,IV,\",\"^6\",\"^7\",\"^8\",null,\"^9\",\"^:\"],[\"^ \",\"^3\",22,\"^4\",\"IV, VIm\",\"^5\",\"~^IV,VIm,\",\"^6\",\"^7\",\"^8\",null,\"^9\",\"^:\"],[\"^ \",\"^3\",23,\"^4\",\"Im, Vm, IVm\",\"^5\",\"~^Im,Vm,IVm,\",\"^6\",\"^7\",\"^8\",null,\"^9\",\"^:\"],[\"^ \",\"^3\",26,\"^4\",\"I, IV, V triads only\",\"^5\",\"~^(?:(?:(?:I)|(?:IV)|(?:V))(?:,|$))*$\",\"^6\",\"^7\",\"^8\",null,\"^9\",\"^:\"],[\"^ \",\"^3\",27,\"^4\",\"I, IV, V, VIm triads only\",\"^5\",\"~^(?:(?:(?:I)|(?:IV)|(?:V)|(?:VIm))(?:,|$))*$\",\"^6\",\"^7\",\"^8\",null,\"^9\",\"^:\"],[\"^ \",\"^3\",28,\"^4\",\"I, IV, V, VIm, IIm triads only\",\"^5\",\"~^(?:(?:(?:I)|(?:IV)|(?:V)|(?:VIm)|(?:IIm))(?:,|$))*$\",\"^6\",\"^7\",\"^8\",null,\"^9\",\"^:\"],[\"^ \",\"^3\",30,\"^4\",\"I,IIm,IIIm,IV,V,VIm,bVII triads only\",\"^5\",\"~^(?:(?:(?:I)|(?:IV)|(?:V)|(?:IIm)|(?:IIIm)|(?:VIm)|(?:bVII))(?:,|$))*$\",\"^6\",\"^7\",\"^8\",null,\"^9\",\"^:\"],[\"^ \",\"^3\",31,\"^4\",\"I,VII\",\"^5\",\"~^I,VII\",\"^6\",\"^7\",\"^8\",null,\"^9\",\"^:\"],[\"^ \",\"^3\",32,\"^4\",\"Chord with a space\",\"^5\",\" \",\"^6\",\"^7\",\"^8\",null,\"^9\",\"~:roman-chords\"],[\"^ \",\"^3\",33,\"^4\",\"Has a power chord\",\"^5\",\"power\",\"^6\",\"^7\",\"^8\",null,\"^9\",\"^<\"],[\"^ \",\"^3\",34,\"^4\",\"Has a sus4 chord\",\"^5\",\"sus4\",\"^6\",\"^7\",\"^8\",null,\"^9\",\"^<\"],[\"^ \",\"^3\",35,\"^4\",\"Has augmented chord\",\"^5\",\"\\\\+\",\"^6\",\"^7\",\"^8\",null,\"^9\",\"^<\"],[\"^ \",\"^3\",36,\"^4\",\"Has a sus2 chord\",\"^5\",\"sus2\",\"^6\",\"^7\",\"^8\",null,\"^9\",\"^<\"],[\"^ \",\"^3\",37,\"^4\",\"Has sus but not sus2\",\"^5\",\"sus[^2]\",\"^6\",\"^7\",\"^8\",null,\"^9\",\"^<\"],[\"^ \",\"^3\",7,\"^4\",\"composer xpath\",\"^5\",\"//creator[@type=\\\"composer\\\"]\",\"^6\",\"~:xpath\",\"^8\",null,\"^9\",\"~:main-musicxml\"],[\"^ \",\"^3\",8,\"^4\",\"Tempo-related Tag\",\"^5\",\"boolean(//measure/sound/@tempo | //measure/direction/sound/@tempo | //measure/direction/direction-type/metronome)\",\"^6\",\"^=\",\"^8\",null,\"^9\",\"^>\"],[\"^ \",\"^3\",10,\"^4\",\"Tempo Tags (not metronome)\",\"^5\",\"boolean(//measure/sound/@tempo | //measure/direction/sound/@tempo)\",\"^6\",\"^=\",\"^8\",null,\"^9\",\"^>\"],[\"^ \",\"^3\",15,\"^4\",\"Has 2nd Part\",\"^5\",\"(//part)[2]/@id\",\"^6\",\"^=\",\"^8\",null,\"^9\",\"^>\"],[\"^ \",\"^3\",21,\"^4\",\"Begins with IIIm\",\"^5\",\"IIIm\",\"^6\",\"~:simple\",\"^8\",\"~:start-with\",\"^9\",\"^:\"],[\"^ \",\"^3\",11,\"^4\",\"Has swing\",\"^5\",\"//words[contains(text(),\'swing\')]/text()\",\"^6\",\"^=\",\"^8\",null,\"^9\",\"^>\"],[\"^ \",\"^3\",16,\"^4\",\"The key is \'none\'\",\"^5\",\"boolean(//key/mode[text()=\'none\'])\",\"^6\",\"^=\",\"^8\",null,\"^9\",\"^>\"]]]]]
+            """
+            .data(using: .utf8)!
+
+        struct Decoded: Codable {
+            let result: Result
+        }
+
+        struct Result: Codable {
+
+        }
+
+        let decoded = try TransitDecoder().decode(Decoded.self, from: data)
+    }
+
 }
