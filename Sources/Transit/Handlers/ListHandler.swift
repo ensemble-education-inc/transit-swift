f3d3f9e2063fb58012057c01d394e09db14cb800
Soroush Khanlou <soroushkhanlou@gmail.com>
2023-05-09 09:58:28 -0400

    update handlers for verbose mode

--- Sources/Transit/Handlers/ListHandler.swift
+++ Sources/Transit/Handlers/ListHandler.swift
@@ -6,17 +6,22 @@
 //
 
 import Foundation
+import OrderedCollections
 
 public struct ListHandler: Handler {
     public func prepareForDecode(value: Any, context: inout Context) throws -> Any {
-        guard let array = value as? [Any] else {
-            return value
+        if let array = value as? [Any] {
+            guard array.first as? String == "~#list" else {
+                return value
+            }
+            _ = try context.transform(value: array[0])
+            return array[1]
+        } else if let dict = value as? OrderedDictionary<String, Any> {
+            if let list = dict["~#list"] {
+                return list
+            }
         }
-        guard array.first as? String == "~#list" else {
-            return value
-        }
-        _ = try context.transform(value: array[0])
-        return array[1]
+        return value
     }
 
     public func prepareForEncode(value: Any, context: inout Context) throws -> Any {
